99bcb4a7b7dcf6a8f746d07703319fe65cdb19a3
Markham Anderson <quinesquines@gmail.com>
2015-07-12 13:05:47 -0700

    Use tagging in fuzzy finder

--- app/models/fuzzy_findable.rb
+++ app/models/fuzzy_findable.rb
@@ -12,24 +12,27 @@ module FuzzyFindable
     end
   end
 
-  def self.autocomplete(search_term, profane=nil)
+  def self.autocomplete(search_term, opts={})
     pattern = FuzzyFindable.fuzzify(search_term)
-    profane = nil unless profane.to_s =~ /1|0|true|false/i
-    query = %Q(SELECT id, name, #{DRINK} AS type
-      #{', profane' unless profane.nil?}
-      FROM drinks WHERE name ILIKE '#{pattern}'
-      #{' AND profane = '+profane.to_s unless profane.nil?}
-      UNION
-      SELECT id, name, #{INGREDIENT} AS type
-      #{', false as profane' unless profane.nil?}
-      FROM ingredients WHERE name ILIKE '#{pattern}'
-      LIMIT #{MAX_RESULTS})
-    cursor = ActiveRecord::Base.connection.execute(query)
-    cursor.to_a
+    selects = []
+    selects.push(fuzzy_sql(DRINK, Drink.table_name, opts[:profane])) unless opts[:drinks] == false
+    selects.push(fuzzy_sql(INGREDIENT, Ingredient.table_name, opts[:profane])) unless opts[:ingredients] == false
+    query = selects.join(' UNION ') + " LIMIT #{MAX_RESULTS}"
+    res = ActiveRecord::Base.connection.raw_connection.exec_params(query, [pattern])
+    res.to_a
   end
 
   def self.fuzzify(search_term)
     "%#{search_term.chars.join('%')}%"
   end
+
+  private
+
+    def self.fuzzy_sql type_id, table_name, profane
+      %Q(SELECT id, name, #{type_id} AS type
+      #{', profane' if profane == false}
+      FROM #{table_name} WHERE name ILIKE $1
+      #{' AND profane = '+profane.to_s if profane == false})
+    end
 
 end
