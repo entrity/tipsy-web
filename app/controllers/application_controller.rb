4137c3b2bc6f3031ad3de607c3195ed0ca7aa270
Markham Anderson <quinesquines@gmail.com>
2015-07-29 18:40:09 -0700

    Change default login redirect to root_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ private
   def after_sign_in_path_for(resource)
     sign_in_url = new_user_session_url
     if request.referer == sign_in_url
-      super
+      root_path
     else
       stored_location_for(resource) || request.referer || root_path
     end
